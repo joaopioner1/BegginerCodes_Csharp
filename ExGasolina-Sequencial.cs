62fd4534f1b2f639a2ea71181deac7942b4e52a9
Joao Vitor Souza Pioner <joaopioner123@gmail.com>
2020-08-12 14:35:33 -0300

    Adicionei o exercício gasolina sequencial.

---
+++ ExGasolina-Sequencial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Transactions;
+
+namespace ExGasolina_Sequencial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira a quantidade de L que seu carro faz por km: ");
+            double lKm = double.Parse(Console.ReadLine());
+            Console.Write("Insira a quilometragem incial do automovel: ");
+            double kmIncial = double.Parse(Console.ReadLine());
+            Console.Write("Insira a quilometagem final do automovel: ");
+            double kmFinal = double.Parse(Console.ReadLine());
+            Console.Write("Insira o valor da gasolina: ");
+            double precoGasolina = double.Parse(Console.ReadLine());
+
+            double media = (kmFinal - kmIncial) / lKm;
+            double precoPagar = media * precoGasolina / 4;
+
+            Console.WriteLine("O total a pagar dividido por todos é: %.2f" + precoPagar.ToString("F2"));
+
+        }
+    }
+}
