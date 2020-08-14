c4441dc1415dc85ff3c00f5d431f68604e8a397e
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-08-14 19:27:11 -0300

    Adicionei o exercício ingresso sequencial.

---
+++ ExIngresso-Sequencial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ExIngresso_Sequencial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o valor do ingresso: ");
+            double valorIngresso = double.Parse(Console.ReadLine());
+            Console.Write("Insira a quantidade de socios: ");
+            int numeroSocios = int.Parse(Console.ReadLine());
+            Console.Write("Insira a quantidade de criancas: ");
+            int numeroCriancas = int.Parse(Console.ReadLine());
+            Console.Write("Insira a quantidade de pessoas comuns: ");
+            int numeroComuns = int.Parse(Console.ReadLine());
+
+            int publicoTotal = numeroComuns + numeroCriancas + numeroSocios;
+            double valorPendente = publicoTotal * valorIngresso;
+            double desconto = valorIngresso * 30.00 / 100 * numeroSocios;
+            double valorTotal = numeroComuns * numeroSocios - desconto;
+
+            Console.WriteLine("Publico total = " + publicoTotal);
+            Console.WriteLine("Valor arrecadado = " + valorTotal);
+            Console.WriteLine("Valor Pendente = " + (valorPendente - valorTotal));
+        }
+    }
+}
