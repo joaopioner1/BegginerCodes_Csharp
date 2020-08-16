5aa0b4232a6100394b3185fe5207b054826d267a
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-08-15 23:48:07 -0300

    Adicionei exercícios sequenciais.

---
+++ ExAntecessor-Sequencial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExAntecessor_Sequencial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com um numero inteiro: ");
+            int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("O numero antecessor = " + (input - 1));
+        }
+    }
+}
---
+++ ExPorcentagem-Sequencial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExPorcentagem_Sequencial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira um numero real: ");
+            double input = double.Parse(Console.ReadLine());
+
+            double porcentagem = input * 20.00 / 100;
+
+            Console.WriteLine("O número acrescentado de 20 por cento = " + (porcentagem + input)); 
+        }
+    }
+}
