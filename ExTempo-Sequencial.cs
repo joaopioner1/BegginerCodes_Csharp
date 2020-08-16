4f5245ab65f9dcedeb8f788df88930a96b56b642
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-08-16 17:15:58 -0300

    Adicionei o exercício tempo sequencial.

---
+++ ExTempo-Sequencial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExTempo_Sequencial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira a velocidade media (km/h): ");
+            double vm = double.Parse(Console.ReadLine());
+            Console.Write("Insira a distancia da viagem: ");
+            double distancia = double.Parse(Console.ReadLine());
+
+            int tempoHoras = (int) distancia / (int) vm;
+            int tempoMinutos = (int) distancia % (int) vm;
+
+            Console.WriteLine("O tempo levado eh: " + tempoHoras + " hr(s) e " + tempoMinutos + " min");
+        }
+    }
+}
