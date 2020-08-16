20ab09cdac51ef4d548c4c8237793e41b310f6c0
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-08-16 17:15:15 -0300

    Adicionei o exercício quadrado sequencial.

---
+++ ExQuadrado-Sequencial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExQuadrado_Sequencial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira um numero: ");
+            double input = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("O quadrado desse numero eh: " + Math.Pow(input, 2));
+        }
+    }
+}
