30d22679fca7dba2568164112173ce2f09b659d7
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-08-16 17:17:29 -0300

    Adicionei o exercício triangulo sequencial.

---
+++ ExTriangulo-Sequencial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExTriangulo_Sequencial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira a base: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Insira a altura: ");
+            double a = double.Parse(Console.ReadLine());
+
+            double area = b * a / 2;
+
+            Console.WriteLine("A area do triangulo eh: " + area.ToString("F2"));
+        }
+    }
+}
