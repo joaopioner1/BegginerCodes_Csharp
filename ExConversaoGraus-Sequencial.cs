b29b52a61dac29f8c6040966b3a01a6fd613848c
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-08-16 17:14:30 -0300

    Adicionei o exercício graus sequencial.

---
+++ ExConversaoGraus-Sequencial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExConversaoGraus_Sequencial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira a temperatura em graus Fahrenheit: ");
+            double input = double.Parse(Console.ReadLine());
+
+            double convert = (input - 32) / 9;
+
+            Console.WriteLine("A temperatura em Celsius eh: " + convert.ToString("F3"));
+        }
+    }
+}
