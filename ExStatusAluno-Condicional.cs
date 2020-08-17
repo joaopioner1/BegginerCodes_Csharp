e8f831d42321bef7c531ac2ee5ec5274fee9054f
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-08-17 12:14:05 -0300

    Adicionei o exercício status aluno condicional.

---
+++ ExStatusAluno-Condicional.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExStatusAluno_Condicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira a nota final do aluno: ");
+            double input = double.Parse(Console.ReadLine());
+
+            if (input >= 7)
+            {
+                Console.WriteLine("O aluno passou!");
+            } else
+            {
+                Console.WriteLine("O aluno reprovou, faltando " + (7 - input) + " pontos para passar.");
+            }
+        }
+    }
+}
