94d430b9bd2042d7b387cb05087c9d280b152eec
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-08-17 12:12:19 -0300

    Adicionei o exercício maior condicional.

---
+++ ExMaior-Condicional.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExMaior_Condicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o primeiro numero: ");
+            double n1 = double.Parse(Console.ReadLine());
+            Console.Write("Insira o segundo numero: ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            if (n1 > n2)
+            {
+                Console.WriteLine("O numero maior eh: " + n1);
+            } else
+            {
+                Console.WriteLine("O numero maior eh: " + n2);
+            }
+        }
+    }
+}
