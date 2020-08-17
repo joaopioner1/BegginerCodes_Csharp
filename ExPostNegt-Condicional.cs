2e34c8f2c12c9c7e9553b94be7743e1d1c51aeb7
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-08-17 12:13:20 -0300

    Adicionei o exercício post negt condicional.

---
+++ ExPostNegt-Condicional.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExPostNegt_Condicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira um numero: ");
+            double input = double.Parse(Console.ReadLine());
+
+            if (input > 0)
+            {
+                Console.WriteLine("O numero eh positivo");
+            } else
+            {
+                Console.WriteLine("O numero eh negativo");
+            }
+        }
+    }
+}
