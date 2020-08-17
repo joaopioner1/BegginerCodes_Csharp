dcc68660a933449ac914b22ba7f7e9bc3ecf46ed
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-08-17 12:11:09 -0300

    Adicionei o exercício divisão condicional.

---
+++ ExDivisao-Condicional.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exdivisao_Sequencial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o nominador: ");
+            double n1 = double.Parse(Console.ReadLine());
+            Console.Write("Insira o denominador: ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            if (n2 > 0 || n2 < 0)
+            {
+                Console.WriteLine("Resultado da divisao = " + n1 / n2);
+            } else
+            {
+                Console.WriteLine("Erro, denominador = 0;");
+            }
+        }
+    }
+}
