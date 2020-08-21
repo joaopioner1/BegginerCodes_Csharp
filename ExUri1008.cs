950e040c99b82a94c86cdd70ae33fc6df02396b3
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-08-21 11:00:04 -0300

    Adc o exercicio 1008 Uri.

---
+++ ExUri1008.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Globalization;
+
+namespace ExUri1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o numero do usario: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o numero de horas trabalhadas: ");
+            int h = int.Parse(Console.ReadLine());
+            Console.Write("Insira quanto o usuario recebe por hora: ");
+            double valorH = double.Parse(Console.ReadLine());
+
+            double salary = valorH * h;
+            Console.WriteLine("NUMBER = " + n);
+            Console.WriteLine("SALARY = U$ " + salary.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
