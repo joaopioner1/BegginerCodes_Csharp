87d1950ecaf3635474430128f8617c5fbdc13f8f
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-08-22 22:47:37 -0300

    Adicionei o exercício multiplo condicional.

---
+++ ExMultiplo-Condicional.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Transactions;
+
+namespace ExMultiplo_Condicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] vet = Console.ReadLine().Split(' ');
+            double a = double.Parse(vet[0]);
+            double b = double.Parse(vet[1]);
+
+            if (a % b == 0 || b % a == 0)
+            {
+                Console.WriteLine("Sao Multiplos.");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos.");
+            }
+        }
+    }
+}
