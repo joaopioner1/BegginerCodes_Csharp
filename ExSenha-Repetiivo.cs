257885668c4c7b346ae49023cecbcf799d335574
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-08-24 23:21:11 -0300

    Adicionei o exercício senha repetitivo.

---
+++ ExSenha-Repetiivo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExSenha_Repetiivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("insira a senha: ");
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Acesso ínválido: ");
+                senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Acesso válido.");
+        }
+    }
+}
