4eb5f4a29a87724c244359faecd216b401a1eb36
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-08-25 11:32:27 -0300

    Adicionei o exercício combustivel repetitivo.

---
+++ ExCombustivel-Repetitivo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ExCombustivel_Repetitivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0;
+            int alcool = 0;
+            int gasolina = 0;
+            int diesel = 0;
+
+            while (input != 4)
+            {
+                input = int.Parse(Console.ReadLine());
+                if (input == 1)
+                {
+                    alcool += 1;
+                } 
+                else if (input == 2)
+                {
+                    gasolina += 1;
+                } 
+                else if (input == 3)
+                {
+                    diesel += 1;
+                }
+            }
+            Console.WriteLine("MUITO OBRIGADO!");
+            Console.WriteLine("Alcool = " + alcool);
+            Console.WriteLine("Gasolina = " + gasolina);
+            Console.WriteLine("Diesel = " + diesel);
+        }
+    }
+}
