52ac39a0698ab85809f8e9b65a788ef41d99a922
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-08-26 12:02:29 -0300

    Adicionei o exercício repetitivo.

---
+++ ExDivisores-Repetitivos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExDivisores_Repetitivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um numero inteiro: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
