3d6d9d2b0a1f350e5923e974eff348acb802af55
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-08-26 12:09:08 -0300

    Adicionei o exercício intervalo in out repetitivo.

---
+++ ExInOut-Repetitivo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExInOut_Repetitivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira a quantidade de números que deseja digitar: ");
+            int n = int.Parse(Console.ReadLine());
+
+            short countIn = 0, countOut = 0;
+            for (int i = 0; i < n; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+                if (input >= 10 && input <= 20)
+                {
+                    countIn += 1;
+                } else
+                {
+                    countOut += 1;
+                }
+            }
+            Console.WriteLine("\n" + countIn + " in");
+            Console.WriteLine(countOut + " out");
+        }
+    }
+          
+}
