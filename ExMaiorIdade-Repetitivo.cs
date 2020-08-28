dbf5d09621dbf22ea46cb268a0a9fd10ac9d886f
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-08-28 10:59:25 -0300

    Adicionei o exercício soma maior idade repetitivo.

---
+++ ExMaiorIdade-Repetitivo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExMaiorIdade_Repetitivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira a quantidade de idades que deseja digitar: ");
+            int input = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+            for (int i = 0; i < input; i++)
+            {
+                int age = int.Parse(Console.ReadLine());
+                if (age >= 18)
+                {
+                    soma += age;
+                }
+            }
+            Console.WriteLine("Soma das idade maiores de 18 = " + soma);
+        }
+    }
+}
