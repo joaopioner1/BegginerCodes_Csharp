425c6f572bf639790b107ee03793e80ae07f10dc
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-08-30 22:04:04 -0300

    Adicionei o exercício employee POO.

---
+++ Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex3POOcurso
+{
+    class Employee
+    {
+        public String name;
+        public double salary;
+    }
+}
