7756408f4fc6c75b0e33711d8262216d3c51df8f
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-08-30 22:01:18 -0300

    Adicionei o exercício pessoa POO.

---
+++ Ex2POOcurso/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex2POOcurso
+{
+    class Pessoa
+    {
+        public int age;
+        public String name;
+    }
+}
---
+++ Ex2POOcurso/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ex2POOcurso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+
+            Console.WriteLine("Dados da primeira pessoa: ");
+            Console.Write("Idade: ");
+            p1.age = int.Parse(Console.ReadLine());
+            Console.Write("Nome: ");
+            p1.name = Console.ReadLine();
+
+            Console.WriteLine("Dados da segunda pessoa: ");
+            Console.Write("Idade: ");
+            p2.age = int.Parse(Console.ReadLine());
+            Console.Write("Nome: ");
+            p2.name = Console.ReadLine();
+
+            if (p1.age > p2.age)
+            {
+                Console.WriteLine("Pessoa mais velha: " + p1.name);
+            } else
+            {
+                Console.WriteLine("Pessoa mais velha: " + p2.name);
+            }
+        }
+    }
+}
